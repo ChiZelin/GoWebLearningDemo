ddf89fa42038d68740c1b58d7bb71239bb87836a
ChiZelin <chizelin@qq.com>
2025-07-30 17:02:55 +0800

    Using Next() and Abort() in middleware, Execution time calculation

--- controllers/helloController.go
+++ controllers/helloController.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 type HelloController struct {
 	BaseController // Embedding BaseController to inherit its methods
@@ -9,6 +13,7 @@ type HelloController struct {
 func (con HelloController) SayHello(c *gin.Context) {
 	con.Success(c)
 	c.String(200, "Hello, this is v1 API!")
+	fmt.Println("SayHello method called")
 }
 
 func (con HelloController) HelloGin(c *gin.Context) {
--- routers/v1Router.go
+++ routers/v1Router.go
@@ -3,12 +3,27 @@ package routers
 import (
 	api "GoWebLearningDemos/controllers"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func initMiddlewares(c *gin.Context) {
+	start := time.Now().UnixNano()
+
 	fmt.Println("Initializing middlewares...")
+
+	c.Next() // Call the next handler in the chain
+
+	//c.Abort() // Abort the chain to prevent further handlers from executing
+
+	fmt.Println("Middlewares initialized.")
+
+	end := time.Now().UnixNano()
+
+	duration := end - start
+
+	fmt.Printf("Execution time: %d nanoseconds\n", duration)
 }
 
 func V1Router(r *gin.Engine) { //Uppercase first character of function name to export it
