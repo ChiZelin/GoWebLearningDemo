fa3f4679f451c74b1c07d972d6a743c0f9c9d1ca
ChiZelin <chizelin@qq.com>
2025-07-30 16:48:31 +0800

    Router middle ware

--- routers/v1Router.go
+++ routers/v1Router.go
@@ -2,14 +2,19 @@ package routers
 
 import (
 	api "GoWebLearningDemos/controllers"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
+func initMiddlewares(c *gin.Context) {
+	fmt.Println("Initializing middlewares...")
+}
+
 func V1Router(r *gin.Engine) { //Uppercase first character of function name to export it
 	v1Router := r.Group("/v1")
 	{
-		v1Router.GET("/hello", api.HelloController{}.SayHello)
-		v1Router.GET("/", api.HelloController{}.HelloGin)
+		v1Router.GET("/hello", initMiddlewares, api.HelloController{}.SayHello)
+		v1Router.GET("/", initMiddlewares, api.HelloController{}.HelloGin)
 	}
 }
